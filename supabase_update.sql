e37761ed21e7501b59c6093b10d77df609821715
amirandavillavicencio <amirandavillavicencio@gmail.com>
2026-03-23 09:48:19 -0300

    Revert "Agregar marcaje de salida"

--- supabase_update.sql
+++
@@ -1,4 +0,0 @@
-alter table public.marcaje_personal
-  add column if not exists hora_entrada timestamp,
-  add column if not exists hora_salida timestamp,
-  add column if not exists estado_entrada text;
